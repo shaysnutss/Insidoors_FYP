12045cceeb21265eb86f1869b9c1e85f4317da9b
shaysnutss <akshayan.2020@scis.smu.edu.sg>
2023-08-21 15:51:18 +0800

    ID-21 Set up pc access service with containerization, and with docker compose

---
+++ pc-access-service/src/test/java/com/service/pcaccessservice/PcAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.pcaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
