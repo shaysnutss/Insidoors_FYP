12df8e05b592c434e971cd6cad60f3bd0bc1b56f
viddyasri_14 <viddyasri@gmail.com>
2023-08-22 20:05:10 +0800

    ID-19 Behavioral Analysis Service

---
+++ behavioral-analysis-service/src/main/java/com/service/behavioralanalysisservice/BehavioralAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.behavioralanalysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BehavioralAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BehavioralAnalysisServiceApplication.class, args);
+	}
+
+}
