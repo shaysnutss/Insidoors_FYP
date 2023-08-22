2fe94536a44306257a6d894b45ccbbf97984b2fb
viddyasri_14 <viddyasri@gmail.com>
2023-08-22 20:05:10 +0800

    ID-19 Behavioral Analysis Service

---
+++ behavioral-analysis-service/src/test/java/com/service/behavioralanalysisservice/BehavioralAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.behavioralanalysisservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BehavioralAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
