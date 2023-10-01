34bd482947ee67e3bf060f7e98f70520c337cb4d
shaysnutss <78736966+shaysnutss@users.noreply.github.com>
2023-10-01 20:47:55 +0800

    Update AuthenticateController.java

--- account-service/src/main/java/com/service/accountservice/auth/AuthenticateController.java
+++ account-service/src/main/java/com/service/accountservice/auth/AuthenticateController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
